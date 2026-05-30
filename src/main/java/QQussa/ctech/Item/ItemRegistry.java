d348eaa7f1c23863230b7d5219d98745f2dc06f3
Mini-Qussa <nbidanov@yandex.ru>
2026-05-30 23:52:10 +0300

    Добавлена регистрация блок айтема block.ctech.wire

--- src/main/java/QQussa/ctech/Item/ItemRegistry.java
+++ src/main/java/QQussa/ctech/Item/ItemRegistry.java
@@ -1,6 +1,7 @@
 package QQussa.ctech.Item;
 
 import QQussa.ctech.Blocks.BlockRegister;
+import QQussa.ctech.Item.Class.CoilClass;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,8 +14,9 @@ import static QQussa.ctech.Ctech.MODID;
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEM = DeferredRegister.create(ForgeRegistries.ITEMS,MODID);
 
+    public static final RegistryObject<BlockItem> SOCKET_ITEM = ITEM.register("socket",() -> new BlockItem(BlockRegister.SOCKET.get(),new Item.Properties()));
     public static final RegistryObject<BlockItem> WIRE_ITEM = ITEM.register("wire",() -> new BlockItem(BlockRegister.WIRE.get(),new Item.Properties()));
-
+    public static final RegistryObject<Item> COIL_ITEM = ITEM.register("coil",() -> new CoilClass(new Item.Properties().stacksTo(1)));
     public static void registry(IEventBus eventBus) {
         ITEM.register(eventBus);
     }
