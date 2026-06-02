6697e08148ec0bff3a2b33ccb1e79eb2bb0a33ac
Mini-Qussa <nbidanov@yandex.ru>
2026-06-02 18:13:19 +0300

    Переработан интерфейс IUniversalEnergy

--- src/main/java/QQussa/ctech/Electrical/IUniversalEnergy.java
+++ src/main/java/QQussa/ctech/Electrical/IUniversalEnergy.java
@@ -1,13 +1,10 @@
 package QQussa.ctech.Electrical;
 
-import net.minecraft.core.BlockPos;
-
-import java.util.Set;
-
 // Специальный интерфейс, для общения между всеми участниками сети
 public interface IUniversalEnergy {
-    RouterElectricalNet getNet();
-    void setNet(RouterElectricalNet net);
-    void updateNet(Set<BlockPos> ignorePos);
-    BlockPos getPos();
+    EnergyBus getNet(); // return block EnergyBus
+
+    boolean setNet(EnergyBus net); // set block EnergyBus
+
+    eType getEnergyType(); // return block eType
 }
