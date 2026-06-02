0dd74cdef95dd4ad15fc9f4b2a704c99d68dadb9
Mini-Qussa <nbidanov@yandex.ru>
2026-06-02 18:06:47 +0300

    Добавлена регистрация NETController.java, а так же NETControllerEntity.java

--- src/main/java/QQussa/ctech/Blocks/BlockRegister.java
+++ src/main/java/QQussa/ctech/Blocks/BlockRegister.java
@@ -1,13 +1,7 @@
 package QQussa.ctech.Blocks;
 
-import QQussa.ctech.Blocks.Class.CreativeAccumulatorClass;
-import QQussa.ctech.Blocks.Class.EnergyBlackHoleClass;
-import QQussa.ctech.Blocks.Class.RouterClass;
-import QQussa.ctech.Blocks.Class.WireClass;
-import QQussa.ctech.Blocks.Entity.CreativeAccumulatorEntity;
-import QQussa.ctech.Blocks.Entity.EnergyBlackHoleEntity;
-import QQussa.ctech.Blocks.Entity.RouterEntity;
-import QQussa.ctech.Blocks.Entity.WireEntity;
+import QQussa.ctech.Blocks.Class.NETController;
+import QQussa.ctech.Blocks.Entity.NETControllerEntity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -20,24 +14,17 @@ import static QQussa.ctech.Ctech.MODID;
 
 public class BlockRegister {
 
-    // Регистрация блоков и сущностей блоков
+    //
     public static final DeferredRegister<Block> BLOCK = DeferredRegister.create(ForgeRegistries.BLOCKS,MODID);
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES,MODID);
 
-    public static final RegistryObject<Block> WIRE = BLOCK.register("wire",() -> new WireClass(BlockBehaviour.Properties.of()));
-    public static final RegistryObject<Block> CREATIVE_ACCUMULATOR = BLOCK.register("creative_accumulator",()->new CreativeAccumulatorClass(BlockBehaviour.Properties.of()));
-    public static final RegistryObject<Block> ENERGY_BLACK_HOLE = BLOCK.register("e_black_hole",()->new EnergyBlackHoleClass(BlockBehaviour.Properties.of()));
-    public static final RegistryObject<Block> ROUTER = BLOCK.register("router_t1",()->new RouterClass(BlockBehaviour.Properties.of()));
+    // -- ALL BLOCK
+    public static final RegistryObject<Block> NET_CONTROLLER = BLOCK.register("net_controller",()-> new NETController(BlockBehaviour.Properties.of()));
+    // -- ALL BLOCK ENTITY
+    public static final RegistryObject<BlockEntityType<NETControllerEntity>> NET_CONTROLLER_ENTITY = BLOCK_ENTITY.register("net_controller",
+            ()-> BlockEntityType.Builder.of(NETControllerEntity::new,NET_CONTROLLER.get()).build(null));
 
-    public static final RegistryObject<BlockEntityType<WireEntity>> WIRE_ENTITY = BLOCK_ENTITY.register("wire_entity"
-            ,() -> BlockEntityType.Builder.of(WireEntity::new,BlockRegister.WIRE.get()).build(null));
-    public static final RegistryObject<BlockEntityType<CreativeAccumulatorEntity>> CREATIVE_ACCUMULATOR_ENTITY = BLOCK_ENTITY.register("creative_accumulator_entity"
-            ,() -> BlockEntityType.Builder.of(CreativeAccumulatorEntity::new,BlockRegister.CREATIVE_ACCUMULATOR.get()).build(null));
-    public static final RegistryObject<BlockEntityType<EnergyBlackHoleEntity>> ENERGY_BLACK_HOLE_ENTITY = BLOCK_ENTITY.register("e_black_hole_entity"
-            ,() -> BlockEntityType.Builder.of(EnergyBlackHoleEntity::new,BlockRegister.ENERGY_BLACK_HOLE.get()).build(null));
-    public static final RegistryObject<BlockEntityType<RouterEntity>> ROUTER_ENTITY = BLOCK_ENTITY.register("router"
-            ,()-> BlockEntityType.Builder.of(RouterEntity::new,BlockRegister.ROUTER.get()).build(null));
-    // Регистрация в шине
+    //
     public static void registry(IEventBus eventBus) {
         BLOCK.register(eventBus);
         BLOCK_ENTITY.register(eventBus);
