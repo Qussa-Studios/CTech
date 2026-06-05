79db7371716122147539ad59843bb69026f7bc1e
Mini-Qussa <nbidanov@yandex.ru>
2026-06-05 16:22:33 +0300

    Create bug_report.m

---
+++ .github/ISSUE_TEMPLATE/bug_report.m
@@ -0,0 +1,36 @@
+---
+name: Bug Report
+about: Report a bug or game crash to help improve the mod
+title: '[BUG] '
+labels: 'type::bug'
+assignees: ''
+---
+
+## Description
+*Provide a clear and concise description of what the bug is.*
+
+---
+
+## Steps to Reproduce
+1. Go to '...'
+2. Click on '...'
+3. See error
+
+---
+
+## Expected Behavior
+*A clear and concise description of what you expected to happen.*
+
+---
+
+## Logs & Screenshots
+* **Crash Report / latest.log:** [Paste link to Pastebin/Gist here, or attach file]
+* *If applicable, add screenshots to help explain your problem.*
+
+---
+
+## Environment
+* **Minecraft Version:** 1.20.1
+* **Forge Version:** *e.g., 47.3.0*
+* **Mod Version:** *e.g., 1.0.0*
+* **Other Mods Installed:** *Are you using OptiFine, Rubidium, Sinytra Connector, etc.?*
